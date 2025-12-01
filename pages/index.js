2048f6d114c97cee7fa132ecd50ce94b90cc1328
isapaula <39596008+isapaula@users.noreply.github.com>
2025-12-01 12:55:56 +0000

    adiciona o `link:check` e `link:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 function Home() {
   return <h1>O sucesso é mais atitude do que aptidão!</h1>;
 }
-// só porque teve uma falha no comando anterior
-// só porque novamente o comando não deu certo.
+
 export default Home;
